8118dbd92154059e00124aa0c782bd9b466b13a5
tiv005 <1475955937@qq.com>
2020-11-03 20:34:38 +0800

    第一次添加文件

---
+++ HelloGit/src/main/java/com/yu/git/HelloGit.java
@@ -0,0 +1,13 @@
+package com.yu.git;
+
+
+
+/**
+ * @author 容
+ * @version 1.0
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
